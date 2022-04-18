19c13961c89d83f4937c00b6bb92af758633575a
moriz <william.dedominico88@gmail.com>
2022-04-18 12:44:26 -0500

    added multiplyer to earnings

--- TreeClicker/ViewController.swift
+++ TreeClicker/ViewController.swift
@@ -26,12 +26,20 @@ class ViewController: UIViewController {
         });
         
         //Initilize dicinarys
+        //tick storage
         tickStorage["Chopper"] = currTick;
         tickStorage["Lumberjack"] = currTick;
         tickStorage["Chainsaw"] = currTick;
         tickStorage["WoodChipper"] = currTick;
         tickStorage["WoodMill"] = currTick;
         tickStorage["WoodComplex"] = currTick;
+        //upgrade storage
+        upgradeStorage["Chopper"] = 1;
+        upgradeStorage["Lumberjack"] = 0;
+        upgradeStorage["Chainsaw"] = 0;
+        upgradeStorage["WoodChipper"] = 0;
+        upgradeStorage["WoodMill"] = 0;
+        upgradeStorage["WoodComplex"] = 0;
     }
 
     func Update() { // Update method is ran every tick
@@ -39,37 +47,37 @@ class ViewController: UIViewController {
         //Chopper
         if (currTick - tickStorage["Chopper"]! >= Chopper.wantedTicks) {
             tickStorage["Chopper"] = currTick;
-            User.money += Chopper.moneyEarned;
+            User.money += Chopper.moneyEarned * upgradeStorage["Chopper"];
         }
         //Luberjack
         if (currTick - tickStorage["Lumberjack"]! >= Lumberjack.wantedTicks) {
             tickStorage["Lumberjack"] = currTick;
-            User.money += Lumberjack.moneyEarned;
+            User.money += Lumberjack.moneyEarned * upgradeStorage["Lumberjack"];
         }
         //Chainsaw
         if (currTick - tickStorage["Chainsaw"]! >= Chainsaw.wantedTicks) {
             tickStorage["Chainsaw"] = currTick;
-            User.money += Chainsaw.moneyEarned;
+            User.money += Chainsaw.moneyEarned * upgradeStorage["Chainsaw"];
         }
         //Wood Chipper
         if (currTick - tickStorage["WoodChipper"]! >= WoodChipper.wantedTicks) {
             tickStorage["WoodChipper"] = currTick;
-            User.money += WoodChipper.moneyEarned;
+            User.money += WoodChipper.moneyEarned * upgradeStorage["WoodChipper"];
         }
         //Wood Mill
         if (currTick - tickStorage["WoodMill"]! >= WoodMill.wantedTicks) {
             tickStorage["WoodMill"] = currTick;
-            User.money += WoodMill.moneyEarned;
+            User.money += WoodMill.moneyEarned * upgradeStorage["WoodMill"];
         }
         //Wood Factory
         if (currTick - tickStorage["WoodFactory"]! >= WoodFactory.wantedTicks) {
             tickStorage["WoodFactory"] = currTick;
-            User.money += WoodFactory.moneyEarned;
+            User.money += WoodFactory.moneyEarned * upgradeStorage["WoodFactory"];
         }
         //Wood Complex
         if (currTick - tickStorage["WoodComplex"]! >= WoodComplex.wantedTicks) {
             tickStorage["WoodComplex"] = currTick;
-            User.money += WoodComplex.moneyEarned;
+            User.money += WoodComplex.moneyEarned * upgradeStorage["WoodComplex"];
         }
         //Update money label
         
