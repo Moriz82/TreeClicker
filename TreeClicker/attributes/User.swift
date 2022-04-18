f814e7ff779d70d01666f27ad4e82f14ec80ce15
Zachary Bauman <zachary.bauman57@k12.leanderisd.org>
2022-04-18 11:54:15 -0500

    Axe,Tree,User Class

---
+++ TreeClicker/attributes/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  TreeClicker
+//
+//  Created by Zachary Bauman on 4/18/22.
+//
+
+import Foundation
+import UIKit
+
+public class User{
+    static var Money:Int=0
+    static var curr_Axe:Axe = Axe(Name: "Wooden Axe", Damage: 10, Image: <#T##UIImage#>)
+    static var curr_Tree:Tree = Tree(Name: "Dirt Tree", Health: 100, Worth: 10, Image: <#T##UIImage#>)
+
+    }
---
+++ TreeClicker/front/Shop.swift
@@ -0,0 +1,13 @@
+//
+//  Shop.swift
+//  TreeClicker
+//
+//  Created by Zachary Bauman on 4/18/22.
+//
+
+import Foundation
+
+public class Shop{
+    let Axes = {1:Axe(Name: "Stone Axe", Damage: 20, Image: <#T##UIImage#>)}
+    
+}
