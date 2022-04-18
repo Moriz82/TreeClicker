c2f61809ca6412a0ecdf23051faf96acae2e817b
moriz <william.dedominico88@gmail.com>
2022-04-18 10:24:49 -0500

    added layout for project

--- TreeClicker/ViewController.swift
+++ TreeClicker/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ticksPerSecond = 12;
+    let currTick = 0;
+    let currSecond = 0;
+    var tickStorage : [String: Int] = [:]
+    var upgradeStorage : [String: [:]] = [:]
+    let timer = Timer();
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad();
+        // Timer for updating based on defined tick rate
+        self.timer = Timer.scheduledTimer(withTimeInterval: (1 / ticksPerSecond), repeats: true, block: { _ in
+            Update();
+            currTick++;
+            currSecond = currTick / ticksPerSecond;
+        });
     }
 
+    func Update() { // Update method is ran every tick
+
+        //Chopper
+
+        if (tickStorage["Chopper"] >= Chopper.wantedTicks()) {
+
+        }
+
+        //Luberjack
+
+        //Chainsaws
+
+        //Wood Chipper
+
+        //Wood Mill
+
+        //Wood Factory
+
+        //Wood Complex
+
+    }
 
 }
 
