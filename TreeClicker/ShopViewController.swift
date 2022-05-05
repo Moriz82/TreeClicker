7e968ad690c174b9db145379c9419f7874121b38
Moriz <william.dedominico88@gmail.com>
2022-05-05 12:23:52 -0500

    sfsdf

--- TreeClicker/ShopViewController.swift
+++ TreeClicker/ShopViewController.swift
@@ -9,63 +9,18 @@ import UIKit
 
 class ShopViewController: UIViewController {
 
-    @IBOutlet weak var axeItemLabel: UILabel!
-    @IBOutlet weak var axeItemButton: UIButton!
+    @IBOutlet weak var labelView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        labelView.layer.cornerRadius = 11;
+        self.view.layer.cornerRadius = 30;
         // display axes
-        var axeLabel:UILabel = axeItemLabel;
-        var axeButton:UIButton = axeItemButton;
         
-        for i in 0...Axes.axes.count-1 {
-            let axe = Axes.axes[i]!
-            axeLabel.text = "\(axe.Name), Level: \(axe.Level), Damage: \(axe.Damage)"
-            axeButton.tag = axe.Level;
-            axeButton.addTarget(self, action: #selector(buyButtonAction), for: .touchUpInside)
-            if i != Axes.axes.count-1 {
-                let oldLabel:CGPoint = axeLabel.frame.origin;
-                let oldButton:CGPoint = axeButton.frame.origin;
-                axeLabel = axeLabel.createCopy();
-                axeButton = axeButton.createCopy();
-                let newPositionLabel:CGPoint = CGPoint(x: oldLabel.x, y: oldLabel.y + 50);
-                let newPositionButton:CGPoint = CGPoint(x: oldButton.x, y: oldButton.y + 50);
-                axeLabel.frame.origin = newPositionLabel;
-                axeButton.frame.origin = newPositionButton;
-                axeLabel.isHidden = false;
-                axeButton.isHidden = false;
-                self.view.addSubview(axeLabel)
-                self.view.addSubview(axeButton)
-            }
-        }
-    }
-    @IBAction func onBackClicked(_ sender: UIButton) {
-        self.navigationController!.popViewController(animated: true)
     }
     
-    @objc func buyButtonAction(sender: UIButton!) {
-        let btnsendtag: UIButton = sender;
-        let selectedAxe:Axe = Axes.axes[btnsendtag.tag]!;
-        if (User.Money >= Double(selectedAxe.Cost)) {
-            User.Money -= Double(selectedAxe.Cost);
-            User.currAxe = selectedAxe;
-        }
+    @IBAction func onExitButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
-
-}
-
-extension UILabel {
-    func createCopy() -> UILabel {
-        let archivedData = NSKeyedArchiver.archivedData(withRootObject: self)
-        return NSKeyedUnarchiver.unarchiveObject(with: archivedData) as! UILabel
-    }
-}
-
-extension UIButton {
-    func createCopy() -> UIButton {
-        let archivedData = NSKeyedArchiver.archivedData(withRootObject: self)
-        return NSKeyedUnarchiver.unarchiveObject(with: archivedData) as! UIButton
-    }
 }
